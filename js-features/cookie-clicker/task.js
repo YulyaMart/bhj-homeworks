2140b1e3d6fcb79a3929d5b1b54a56bb12213c2f
YulyaMart <120514559+YulyaMart@users.noreply.github.com>
2023-09-28 13:22:01 +0300

    task.js shortened code added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,12 +2,18 @@ const clicker__counter = document.getElementById('clicker__counter')
 const img_cookie = document.getElementById('cookie')
 
 img_cookie.onclick = () => {
-    clicker__counter.textContent = Number(clicker__counter.textContent) + 1
-    if (clicker__counter.textContent % 2) {
-        img_cookie.width += 20
-        img_cookie.height += 20
-    } else {    
-        img_cookie.width -= 20
-        img_cookie.height -= 20
-    }
+
+    img_cookie.width = ++clicker__counter.textContent % 2 ? 250 : 200;
+
 }
+
+// img_cookie.onclick = () => {
+//     clicker__counter.textContent = Number(clicker__counter.textContent) + 1
+//     if (clicker__counter.textContent % 2) {
+//         img_cookie.width += 20
+//         img_cookie.height += 20
+//     } else {    
+//         img_cookie.width -= 20
+//         img_cookie.height -= 20
+//     }
+// }
