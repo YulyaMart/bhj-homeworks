4d0882dee663c0bb0ffa7c4f0ad3d2e5ee48218d
YulyaMart <120514559+YulyaMart@users.noreply.github.com>
2023-09-28 14:04:13 +0300

    function added

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -6,18 +6,19 @@ for (let i=1; i<10; i++) {
     getHole(i).onclick = () => {
         if (getHole(i).className.includes( 'hole_has-mole' )) {
             dead.textContent = Number(dead.textContent) + 1
-            if (dead.textContent == '10') {
-                alert('Победа!')
+            let gameResult = function(alertText) {
+                alert(alertText)
                 dead.textContent = 0
                 lost.textContent = 0
+            }
+            if (dead.textContent == '10') {
+                gameResult('Победа!')
             }
         }
         else {
             lost.textContent = Number(lost.textContent) + 1
             if (lost.textContent == '5') {
-                alert('Вы проиграли!')
-                dead.textContent = 0
-                lost.textContent = 0
+                gameResult('Вы проиграли!')
             }
         }
     }
